738e74b5425ca926559486ad9d75c6c1de7f196e
Sebastián Palacio <sebalp_1987@hotmail.com>
2019-02-05 12:59:49 +0100

    sup_models

---
+++ model/neural_net.py
@@ -0,0 +1,62 @@
+from keras.utils import to_categorical
+from keras import layers, Input, regularizers
+from keras.models import Model
+from keras.optimizers import SGD, Adam
+from keras.callbacks import EarlyStopping
+
+
+class NeuralNetwork(object):
+
+    def __init__(self, n_cols, node_size=[100],
+                 prob_dropout=None, sparsity_const=10e-5, activation='relu', beta=1):
+        """
+        :param n_cols: Number of Predictors
+        :param node_size: Node Size (but also the len(node_size) determines the number of layers)
+        :param prob_dropout: proportion to dropout
+        :param sparsity_const: Restrict some nodes and not all (as PCA), using regularization strategy
+        :param activation: Activation function
+        """
+
+        self.n_cols = n_cols
+        self.activation = activation
+        self.prob_dropout = prob_dropout
+        self.node_size = node_size
+        self.sparsity_const = sparsity_const
+        self.beta = beta
+
+        input_layer = Input(shape=(n_cols,))
+
+        x = input_layer
+
+        for node in node_size:
+            if sparsity_const is not None:
+                x = layers.Dense(node, activation=self.activation,
+                                 activity_regularizer=regularizers.l1(sparsity_const))(x)
+            else:
+                x = layers.Dense(node, activation=self.activation)(x)
+
+            if self.prob_dropout is not None:
+                x = layers.Dropout(self.prob_dropout)(x)
+
+        output_tensor = layers.Dense(2, activation='softmax')(x)
+
+        self.model = Model(input_layer, output_tensor)
+
+        print(self.model.summary())
+
+    def fit_model(self, predictors, target, learning_rate=0.001, loss_function='categorical_crossentropy', epochs=500,
+                  batch_size=500, verbose=True, validation_data=None):
+
+        target = to_categorical(target)
+        if validation_data is not None:
+            validation_data[1] = to_categorical(validation_data[1].target)
+        callback_list = EarlyStopping(patience=2)
+        optimizer = Adam(lr=learning_rate)
+        self.model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
+        self.model.fit(x=predictors, y=target, epochs=epochs, batch_size=batch_size,
+                       validation_data=validation_data,
+                       callbacks=[callback_list], verbose=verbose)
+
+    def predict_model(self, x_test):
+        return self.model.predict(x_test)
+
--- model/sup_models.py
+++ model/sup_models.py
@@ -4,30 +4,34 @@ import numpy as np
 import matplotlib.pyplot as plot
 import seaborn as sns
 
+from resources.sampling import over_sampling, under_sampling
+from model.neural_net import NeuralNetwork
+
 from sklearn.model_selection import StratifiedKFold
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.metrics import recall_score, precision_score, fbeta_score, confusion_matrix
 
-from resources.sampling import over_sampling, under_sampling
-
-# Parameters
-sns.set()
-tresholds = np.linspace(0.1, 1.0, 200)
-scores = []
-sampling = None
-label = 'target'
-
-# Models
-fileModel = ExtraTreesClassifier(n_estimators=100, max_depth=50, bootstrap=False, oob_score=False,
-                                 class_weight='balanced_subsample')
-
 # Files
+label = 'target'
 train = pd.read_csv(STRING.train, sep=';', encoding='latin1')
 valid = pd.read_csv(STRING.valid, sep=';', encoding='latin1')
 test = pd.read_csv(STRING.test, sep=';', encoding='latin1')
 test = pd.concat([train, valid, test], axis=0).reset_index()
 x = test.drop([label] + ['oferta_id'], axis=1)
 y = test[[label]]
+
+
+# Parameters
+sns.set()
+tresholds = np.linspace(0.1, 1.0, 200)
+scores = []
+sampling = None
+cols = x.shape[1]
+# Models
+fileModel1 = ExtraTreesClassifier(n_estimators=100, max_depth=50, bootstrap=False, oob_score=False,
+                                 class_weight='balanced_subsample')
+fileModel = NeuralNetwork(n_cols=cols, node_size=[100], activation='relu', prob_dropout=0.2)
+
 
 # We define the stratify folds
 y_pred_score = np.empty(shape=[0, 2])
@@ -36,6 +40,7 @@ skf = StratifiedKFold(n_splits=5, random_state=42, shuffle=False)
 
 # For each Fold
 for train_index, test_index in skf.split(x.values, y[label].values):
+    print('1')
 
     x_train, x_test = x.loc[train_index].values, x.loc[test_index].values
     y_train, y_test = y.loc[train_index].values, y.loc[test_index].values
@@ -53,8 +58,16 @@ for train_index, test_index in skf.split(x.values, y[label].values):
     min_sample_split = min_sample_leaf * 10
     max_features = round(x_train.shape[1] / 3)
 
-    fileModel.fit(x_train, y_train)
-    y_pred_score_i = fileModel.predict_proba(x_test)
+    try:
+        fileModel.fit(x_train, y_train)
+        y_pred_score_i = fileModel.predict_proba(x_test)
+    except:
+        fileModel.fit_model(x_train, y_train, learning_rate=0.001, loss_function='cosine_proximity',
+                            epochs=100,
+                            batch_size=1000, verbose=True, validation_data=None)
+        y_pred_score_i = fileModel.predict_model(x_test)
+        print(y_pred_score_i)
+
     y_pred_score = np.append(y_pred_score, y_pred_score_i, axis=0)
     predicted_index = np.append(predicted_index, test_index, axis=0)
     del x_train, x_test, y_train, y_test
